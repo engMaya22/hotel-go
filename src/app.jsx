d4d8d6510b236d4743695aa51df08be6a63269d8
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-09-16 02:03:23 +0800

    nav bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./index.css"
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
+
 export const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+
   return (
-    <div className='text-red-700'>
-      test
+    <div className=''>
+     { !isOwnerPath && <Navbar />}
     </div>
   )
 }
