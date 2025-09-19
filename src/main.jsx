447e0face2c92aae5a332d6458c4c3b0184f0826
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-09-20 00:20:19 +0800

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,17 @@ import { render } from 'preact'
 import './index.css'
 import { App } from './app.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+
+render(<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+     <BrowserRouter> <App /> </BrowserRouter>
+    </ClerkProvider>, document.getElementById('app'))
