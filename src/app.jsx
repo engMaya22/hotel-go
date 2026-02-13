df48875a2c3110f72ba89363a76520af8fb6915a
engMaya22 <eng.maya.esmaeel1@gmail.com>
2026-02-14 01:56:34 +0800

    bookings page

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 export const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -20,6 +21,7 @@ export const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,111 @@
+
+
+import React, { useEffect, useState } from 'react'
+import Title from '../components/Title'
+import { assets, userBookingsDummyData } from '../assets/assets'
+
+const MyBookings = () => {
+
+    const [bookings, setBookings] = useState(userBookingsDummyData);
+    useEffect(() => {
+
+        // here we need to set booking based on current user
+    });
+
+
+    return (
+        <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32 '>
+            <Title title='My Bookings' subtitle='View and manage all your upcoming and past reservations in one place.' align='left' />
+
+            <div className='max-w-6xl w-full mt-8 text-gray-800'>
+                <div className='hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b font-medium text-base border-gray-300 py-3'>
+                    <div className='w-1/3'>
+                        Hotels
+                    </div>
+                    <div className='w-1/3'>
+                        Date & Timing
+                    </div>
+                    <div className='w-1/3'>
+                        payment
+                    </div>
+                </div>
+
+                <div className=''>
+                    {bookings.map((booking) =>
+                        <div key={booking._id} className='grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full py-6 border-gray-300 border-b 
+                                   first:border-t'>
+                            {/* hotel details */}
+                            <div className='flex flex-col md:flex-row'>
+                                <img src={booking.room.images[0]} alt="hotel-img" className='min-md:w-44 rounded shadow object-cover' />
+                                <div className='flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4'>
+                                    <p className='font-playfair text-2xl'>
+                                        {booking.hotel.name}
+
+                                        <span className='text-sm font-inter'>({booking.room.roomType})</span>
+                                    </p>
+                                    <div className='flex items-center gap-1 text-sm text-gray-500'>
+                                        <img src={assets.locationIcon} alt="location-icon" />
+                                        {booking.hotel.address}
+
+                                    </div>
+
+                                    <div className='flex items-center gap-1 text-sm text-gray-500'>
+                                        <img src={assets.guestsIcon} alt="guests-icon" />
+                                        {booking.guests}
+
+                                    </div>
+                                    <p className='text-base'>
+                                        Total:  ${booking.totalPrice}
+                                    </p>
+                                </div>
+                            </div>
+                            {/* dates && timing */}
+                            <div className='flex flex-row gap-8 md:gap-12 md:items-center mt-3'>
+                                <div>
+                                    <p>
+                                        Check-In:
+                                    </p>
+                                    <p className='text-gray-500 text-sm'>
+                                        {new Date(booking.checkInDate).toDateString()}
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <p>
+                                        Check-Out:
+                                    </p>
+                                    <p className='text-gray-500 text-sm'>
+                                        {new Date(booking.checkOutDate).toDateString()}
+                                    </p>
+                                </div>
+
+                            </div>
+
+                            {/* payment status */}
+                            <div className='flex flex-col items-start justify-center pt-3'>
+                                <div className="items-center gap-2 flex">
+                                    <div className={` h-3 w-3  rounded-full ${booking.isPaid ? 'bg-green-500' : 'bg-red-500'}`}>
+
+                                    </div>
+                                    <p className={` text-sm ${booking.isPaid ? 'text-green-500' : 'text-red-500'}`}>
+                                        {booking.isPaid ? "Paid" : "Unpaid"}
+                                    </p>
+                                </div>
+                                {!booking.isPaid && (
+                                    <button className='px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full transition-all cursor-pointer hover:bg-gray-500'>
+                                        Pay Now
+                                    </button>
+                                )
+                                }
+
+                            </div>
+
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyBookings
