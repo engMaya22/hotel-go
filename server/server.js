57315b5ba84a1447500ed4c338ee14c4e14eb0fe
engMaya22 <eng.maya.esmaeel1@gmail.com>
2026-02-23 00:24:59 +0800

    stracture for backend

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+connectDB();
+const app = express();
+app.use(cors());//enable cross-origin resource sharing
+app.get('/',(req , res)=> res.send("Api is working"))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT , ()=>console.log(`Server running on port  ${PORT}`))
