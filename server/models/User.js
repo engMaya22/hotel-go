7a0327c58ebe6f8329d638aec5830f2f3e4509ad
engMaya22 <eng.maya.esmaeel1@gmail.com>
2026-02-24 00:52:48 +0800

    fix

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
    _id : {type:String , required:true},
-   username : {type:String , required:true},
+   user_name : {type:String , required:true},
    email : {type:String , required:true},
    image : {type:String , required:true},
    role : {type:String , enum : ["user",'hotelOwner'], default:"user"},
